ada606490e6d4696f57bbffdaf865298e78659e7
marcelogaldino <marcelinhomgo@gmail.com>
2021-10-19 23:01:38 -0300

    feat: prisma and socket.io implemented, initialized web

---
+++ backend-node/src/server.ts
@@ -0,0 +1,5 @@
+import { serverHttp } from "./app"
+
+serverHttp.listen(4000, () => {
+    console.log('🚀️ Server is running on port 4000')
+})
---
+++ web/src/App.tsx
@@ -0,0 +1,8 @@
+export function App() {
+  return (
+    <main className="content-wrapper">
+      <h1>Hello</h1>
+    </main>
+  )
+}
+
